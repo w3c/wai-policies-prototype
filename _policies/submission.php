d8d0c90c7134763cee3f2c41d0fb605f2cc8a439
Kevin White <kevin@w3.org>
2023-02-10 11:53:21 +0000

    Fixes to submission page
    * Fix return to submission form link
    * Update last changed date
    * Update editors
    * Update copyright statement

--- _policies/submission.php
+++ _policies/submission.php
@@ -20,7 +20,7 @@ EOF;
 
 $github_token = "@@@@@@@"; // Do not check token into the repository
 
-if (!$_POST)  { echo ('<h1>Go to submission form</h1><p>To submit a policy, <a href="https://w3c.github.io/wai-policies-prototype/submission.html">return to the submission form</a>.</p>'); } else {
+if (!$_POST)  { echo ('<h1>Go to submission form</h1><p>To submit a policy, <a href="https://www.w3.org/WAI/policies/submission/">return to the submission form</a>.</p>'); } else {
 if (trim($_POST['comment'])) { die("This may be spam."); } // If someone enters text into the honeypot, stop form submission
 
 $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
@@ -201,16 +201,14 @@ $return = json_decode(curl_exec($ch));
 </div>
 <div id="footer">
   <h2>Document Information</h2>
-  <p><strong>Content last updated:</strong> 2017<br />
-    Editors: Mary Jo Mueller (IBM) and Robert Jolly (Knowbility). Staff support: Eric Eggert (W3C/Knowbility).<br />
+  <p><strong>Content last updated:</strong> 2023<br />
+    Editors: Vera Lange (HAN University), Michel Hansma (HAN University) and Eric Velleman (HAN University). Staff support: Kevin White (W3C).<br />
+    Previous editors: Mary Jo Mueller (IBM) and Robert Jolly (Knowbility). Staff support: Eric Eggert (W3C/Knowbility).<br />
     Previous editors: Judy Brewer (W3C) and Shawn Lawton Henry (W3C). Developed with the Education and Outreach Working
     Group (<a href="https://www.w3.org/WAI/EO/">EOWG</a>).</p>
   <div class="footer-nav"><p>[<a href="#disclaimer-this-is-not-legal-advice">Disclaimer</a>] [<a href="https://www.w3.org/WAI/sitemap.html">WAI Site Map</a>] [<a href="https://www.w3.org/WAI/sitehelp.html">Help with WAI Website</a>] [<a href="https://www.w3.org/WAI/search.php">Search</a>] [<a href="https://www.w3.org/WAI/contacts">Contacting WAI</a>]<br />
          <strong>Feedback welcome to <a href="mailto:wai-eo-editors@w3.org">wai-eo-editors@w3.org</a></strong> (a publicly archived list) or <a href="mailto:wai@w3.org">wai@w3.org</a> (a WAI staff-only list).</p></div><div class="copyright">
-            <p><a rel="Copyright" href="https://www.w3.org/Consortium/Legal/ipr-notice#Copyright">Copyright</a> © 1994-2017 <a href="https://www.w3.org/"><abbr title="World Wide Web Consortium">W3C</abbr></a><sup>®</sup> (<a href="http://www.csail.mit.edu/"><abbr title="Massachusetts Institute of Technology">MIT</abbr></a>, <a href="http://www.ercim.org/"><abbr title="European Research Consortium for Informatics and Mathematics">ERCIM</abbr></a>, <a href="http://www.keio.ac.jp/">Keio</a>), All Rights Reserved. W3C <a href="https://www.w3.org/Consortium/Legal/ipr-notice#Legal_Disclaimer">liability</a>, <a href="https://www.w3.org/Consortium/Legal/ipr-notice#W3C_Trademarks">trademark</a>, <a rel="Copyright" href="https://www.w3.org/Consortium/Legal/copyright-documents">document use</a> and <a rel="Copyright" href="https://www.w3.org/Consortium/Legal/copyright-software">software
-         licensing</a> rules apply. Your interactions with this site are in
-         accordance with our <a href="https://www.w3.org/Consortium/Legal/privacy-statement#Public">public</a> and <a href="https://www.w3.org/Consortium/Legal/privacy-statement#Members">Member</a> privacy
-         statements.</p>
+            <p>Copyright © 2023 World Wide Web Consortium (<a href="https://www.w3.org/">W3C</a><sup>®</sup>). See <a href="/WAI/about/using-wai-material/">Permission to Use WAI Material</a>.</p>
          </div>
 <!-- end footer --></div>
 <?php wai_analytics(); ?>
